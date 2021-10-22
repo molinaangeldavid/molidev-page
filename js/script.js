75084f459a9383de9e4d5f2ef4094861eb981248
molinaangeldavid <molina.angeldavid@gmail.com>
2021-10-21 23:32:32 -0300

    Agregando diseño responsive y boton lenguaje

--- js/script.js
+++ js/script.js
@@ -1,6 +1,24 @@
 const icons = document.querySelectorAll('.icon');
+const navUl = document.getElementsByClassName("menu-flex")
 icons.forEach (icon => {  
     icon.addEventListener('click', (event) => {
     icon.classList.toggle("open");
     });
+
 });
+
+
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.maxHeight) {
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + "px";
+    } 
+  });
+}
